f7b0087fb0319c9ef6ea102efa724b426bf3b634
Patrick <mcbridep212@gmail.com>
2021-01-05 21:15:32 -0500

    Data Augmentation and Dropout now included

--- main.py
+++ main.py
@@ -135,7 +135,8 @@ def main():
     start_time = time.time()
     # beginning construction of the Convnet
     model = models.Sequential()
-    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
+    model.add(layers.Conv2D(32, (3, 3), activation='relu',
+                            input_shape=(150, 150, 3)))
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Conv2D(64, (3, 3), activation='relu'))
     model.add(layers.MaxPooling2D((2, 2)))
@@ -144,39 +145,46 @@ def main():
     model.add(layers.Conv2D(128, (3, 3), activation='relu'))
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Flatten())
+    model.add(layers.Dropout(0.5))
     model.add(layers.Dense(512, activation='relu'))
     model.add(layers.Dense(1, activation='sigmoid'))
 
-    # model compilation
     model.compile(loss='binary_crossentropy',
                   optimizer=optimizers.RMSprop(lr=1e-4),
                   metrics=['acc'])
 
     # data reading and formatting
-    train_datagen = ImageDataGenerator(rescale=1./255)
-    test_datagen = ImageDataGenerator(rescale=1./255)
+    train_datagen = ImageDataGenerator(
+        rescale=1. / 255,
+        rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True, )
+
+    test_datagen = ImageDataGenerator(rescale=1. / 255)
 
     train_generator = train_datagen.flow_from_directory(
         train_dir,
         target_size=(150, 150),
-        batch_size=20,
+        batch_size=32,
         class_mode='binary')
 
     validation_generator = test_datagen.flow_from_directory(
         validation_dir,
         target_size=(150, 150),
-        batch_size=20,
+        batch_size=32,
         class_mode='binary')
 
-    # Model Training
-    history = model.fit_generator(
+    history = model.fit(
         train_generator,
-        steps_per_epoch=100,
-        epochs=30,
+        steps_per_epoch=50,
+        epochs=100,
         validation_data=validation_generator,
-        validation_steps=50)
+        validation_steps=10)
 
-    model.save('cats_and_dogs_small_1.h5')
+    model.save('cats_and_dogs_small_2.h5')
 
     # Plotting the results
     plot_history(history)
